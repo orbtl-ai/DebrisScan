6835092425d052b3df380d7b4525c521ff84fa4a
Ross Winans <ross.winans@gmail.com>
2022-09-06 12:46:59 -0400

    updated status/delivery complete

--- app/client/app.py
+++ app/client/app.py
@@ -77,7 +77,7 @@ def async_object_detection(
     return {
         upload_results: gr.update(visible=True),
         out_payload: str(task_id),
-        out_message: "Upload Successful! However it may take awhile to count all that debris. Luckily you don't need to wait around for us! Please save your Task ID and return at any time to check your Task Status at the tab above!"
+        out_message: "Upload Successful! However it may take us awhile to count all that debris!! Luckily you don't need to wait around for us! Please save your Task ID and return at any time to check your Task Status at the tab above!"
     }
 
 
@@ -96,20 +96,50 @@ async def get_task_status(task_id):
 
     result_state = str(result.state)
     result_error = str(result.info) if result.failed() else None
+
+    # result.get() can block the whole thread if nothing is there... use with caution
     result_file = str(result.get()) if result.state == states.SUCCESS else None
 
     if result_error is None and result_state == "PENDING":
-        out_status = "PENDING: Your submission has been received and is currently \
-        waiting in a queue for processing. Please check back later."
+        out_message = "PENDING: Your submission has been received and is currently waiting in a queue for processing. Please check back later."
+
+        return {
+            out_status: gr.update(value=out_message, visible=True),
+            out_file: gr.update(visible=False),
+        }
+
     elif result_error is None and result_state == "STARTED":
-        out_status = "STARTED: Your submission is currently being processed."
+        out_message = "STARTED: Your submission is currently being processed."
+
+        return {
+            out_status: gr.update(value=out_message, visible=True),
+            out_file: gr.update(visible=False),
+        }
+
+    elif result_error is None and result_state == "SUCCESS":
+        out_message = "SUCCESS: Your submission has been processed successfully! Please click the 'Download Results' button to the right to download your results."
+
+        return {
+            out_status: gr.update(value=out_message, visible=True),
+            out_file: gr.update(value=result_file, visible=True),
+        }
+
     elif result_error is not None:
-        out_status = f"{result.id} HAS FAILED!. {str(result_error)}."
+        out_message = f"{result.id} HAS FAILED!. {str(result_error)}."
+
+        return {
+            out_status: gr.update(value=out_message, visible=True),
+            out_file: gr.update(visible=False),
+        }
+
     else:
-        out_status = f"{result.id}'s job status can not be retrieved reliably... \
+        out_message = f"{result.id}'s job status can not be retrieved reliably... \
             please contact the project's administrators."
 
-    return {out_status}
+        return {
+            out_status: gr.update(value=out_message, visible=True),
+            out_file: gr.update(visible=False),
+        }
 
 
 async def get_task_results(task_id: str):
@@ -162,9 +192,10 @@ def toggle_resampling(choice):
             in_sensor_platform: gr.update(visible=False),
         }
 
+
 browser_title = "DebrisScan Demo"
 
-demo_title = "# Welcome to the DebrisScan Debris Detection Demo!"
+demo_title = "# Welcome to DebrisScan"
 
 demo_description = """
     **DebrisScan is an AI-based tool that automatically detects, classifies, and measures
@@ -195,17 +226,16 @@ with gr.Blocks(title=browser_title) as demo:
                 gr.Markdown("## Optional Settings")
                 with gr.Column():
                     gr.Markdown("""
-                        ### Auto-resample aerial images to 2cm resolution?
-                        *Resampling requires us to know more about your imagery, but it
-                        should improve your detection results by ensuring your aerial
-                        images match our AI's expectations (they can be a little picky-
-                        HIGHLY RECOMMENDED).*
+                        ### Auto-resample your aerial images to match the AI's expected resolution?
+                        *Auto-resampling requires us to know more about your imagery,
+                        but it should improve your detection results by ensuring your
+                        aerial image resolution matches the image resolution used to
+                        train our AIs (they can be a little picky- so this is HIGHLY
+                        RECOMMENDED).*
                     """)
                     in_resampling = gr.Checkbox(
-                            label=f"Automatically resample aerial images to \
-                                {int(api_configs.TARGET_GSD_CM)}cm resolution?\
-                                If so, we will also need to know the images' flight \
-                                altitude and camera parameters.",
+                            label=f"Auto-resample your imagery to \
+                                {int(api_configs.TARGET_GSD_CM)}cm?"
                     )
                     in_flight_agl = gr.Slider(
                         label="Flying Height Above Ground Level (meters)",
@@ -262,20 +292,31 @@ with gr.Blocks(title=browser_title) as demo:
                     in_sensor_platform,
                     confidence_threshold,
                 ],
-                # outputs=[out_task_id],
                 outputs=[upload_results, out_payload, out_message],
             )
 
     with gr.Tab("Retrieve Task Status"):
         gr.Markdown("## Enter Task ID")
         with gr.Column():
-            in_task_id = gr.Text(label="Task ID")
-            status_button = gr.Button(value="Get Job Status")
-            out_status = gr.Text(label="Task Status")
+            in_task_id = gr.Text(label="Task ID", show_label=False)
+            status_button = gr.Button(value="Get Task Status")
+
+        gr.Markdown("## Task Status")
+        with gr.Row():
+            out_status = gr.Text(
+                label="Task Status",
+                show_label=False,
+                value="Awaiting Task ID above...",
+            )
+            out_file = gr.File(
+                label="Download Your Results!",
+                value=None,
+                visible=False,
+            )
         status_button.click(
             get_task_status,
             inputs=[in_task_id],
-            outputs=[out_status],
+            outputs=[out_status, out_file],
         )
     gr.Markdown(demo_article)
 
