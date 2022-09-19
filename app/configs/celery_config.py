e8cea7caf4e9c8948896a5059da3c7a394970510
Ross Winans <ross.winans@gmail.com>
2022-09-19 11:55:49 -0400

    changed internal naming of dart-api to debrisscan

--- app/configs/celery_config.py
+++ app/configs/celery_config.py
@@ -5,7 +5,7 @@ print(f"CELERY_BROKER_URL: {getenv('CELERY_BROKER_URL')}")
 print(f"CELERY_RESULT_BACKEND: {getenv('CELERY_RESULT_BACKEND')}")
 print(f"TASK_TRACK_STARTED: {getenv('CELERY_TASK_TRACK_STARTED')}")
 
-celery_app_name: str = getenv('CELERY_APP_NAME', 'dart-api')
+celery_app_name: str = getenv('CELERY_APP_NAME', 'debrisscan')
 broker_url: str = getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
 result_backend: str = getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
 task_track_started: bool = getenv('CELERY_TASK_TRACK_STARTED', True)
